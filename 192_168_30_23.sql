ad7cfb2db209a3e519d2236d48579efed7baa529
Anna Shkarbun <146517961+onnuna@users.noreply.github.com>
2024-05-20 18:20:22 +0200

    Add files via upload

---
+++ 192_168_30_23.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 6.0.0-dev
+-- https://www.phpmyadmin.net/
+--
+-- Host: 192.168.30.23
+-- Czas generowania: 20 Maj 2024, 16:19
+-- Wersja serwera: 8.0.18
+-- Wersja PHP: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `go_game_db`
+--
+CREATE DATABASE IF NOT EXISTS `go_game_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `go_game_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wyniki`
+--
+
+CREATE TABLE `wyniki` (
+  `id` int(11) NOT NULL,
+  `gracz1` varchar(50) NOT NULL,
+  `gracz2` varchar(50) NOT NULL,
+  `zwyciezca` varchar(50) NOT NULL,
+  `data_gry` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `wyniki`
+--
+ALTER TABLE `wyniki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `wyniki`
+--
+ALTER TABLE `wyniki`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
